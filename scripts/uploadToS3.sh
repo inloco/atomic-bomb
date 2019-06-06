a35e25cb561f8b03c3f182dbb3f2bd338c3852b4
Gileno Oliveira <gileno.oliveira@inlocomedia.com>
2019-06-06 14:19:35 -0300

    chore(cdn): Add circle ci build and deploy to cdn

---
+++ scripts/uploadToS3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+oneDay="84600"
+folder="$1"
+bucket="$2"
+
+find build -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
+
+aws s3 sync $folder s3://$bucket --exclude "*.*" --include "*.js" --include "*.css" --acl public-read --cache-control "max-age=$oneDay" --content-encoding "gzip"
